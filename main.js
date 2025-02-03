b1a79662d55d74cd2f910c9ddd258370923e0bec
ZakhaevK <zkupke@gmail.com>
2025-02-04 01:33:18 +1100

    Add fibs and fibsRec for fibonacci arrays

--- main.js
+++ main.js
@@ -23,3 +23,31 @@ function fibRec(n) {
 
 console.log(fib(8));
 console.log(fibRec(8));
+
+
+function fibs(n) {
+  let results = [];
+  for (let i = 0; i < n; i++) {
+
+    if (i < 2) {
+      results.push(i);
+      continue
+    }
+    results.push(results[i - 1] + results[i - 2])
+  }
+  return results;
+}
+
+
+function fibsRec(n) {
+
+  if (n == 0) {
+    return [];
+ }
+// Return chain begins at n == 0 is true, then the concat begins from [].concat(fibRec(1))
+// starting the array at [0] and proceeding from there 
+ return fibsRec(n - 1).concat(fibRec(n));
+}
+
+console.log(fibs(8));
+console.log(fibsRec(8));
